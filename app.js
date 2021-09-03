c5006509172585ea6539e873545da2ffe9c4c341
Raiee Gulhane <64693025+raieegulhane@users.noreply.github.com>
2021-09-03 23:18:37 +0530

    [refact]: edited comments

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Selecting all queries
 var buttonClick = document.querySelector(".btn-translate");
 var textInput = document.querySelector(".text-input");
 var textOutput = document.querySelector(".text-output");
@@ -6,19 +5,17 @@ var textOutput = document.querySelector(".text-output");
 // Server URL
 var url = "https://api.funtranslations.com/translate/minion.json";
 
-// Function to add the text in input to URL
+// Add the text in input to URL
 function editURL(text) {
     return url + "?text=" + text;
 }
 
-// For handling errors when server cannot be reached or is down
+// Handling errors 
 function errorHandler(error) {
     console.log("Error occured", error);
     alert("Something went wrong. Can't reach the server. Try again.");
 }
 
-// Function that tells what to do when button is clicked 
-// (processing on button click)
 function clickListener () {
     // Storing the input typed by user
     var inputTxt = textInput.value;
@@ -33,10 +30,8 @@ function clickListener () {
             textOutput.innerText = translatedOutput;
         })
     
-        // This handles the error
         .catch(errorHandler)
 }
 
 
-// On click do this
 buttonClick.addEventListener ("click", clickListener);
